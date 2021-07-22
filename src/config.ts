dabf14ec7e7a97f1e921f08c8e9dbdafc4d763af
Vova <vovadorko@gmail.com>
2021-07-22 13:19:44 +0300

    feat: version check && README was extanded

--- src/config.ts
+++ src/config.ts
@@ -1,28 +1,33 @@
 import { getRandomSubdomain } from './services/random-subdomain';
 import { logger } from '@leanylabs/logger';
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs');
+const argv = yargs(hideBin(process.argv)).argv;
 
 const getSubdomain = () => {
   const { TUNNEL_SUBDOMAIN } = process.env;
 
-  if (!TUNNEL_SUBDOMAIN) {
-    logger.warn('TUNNEL_SUBDOMAIN is missed, using a random subdomain');
-    return getRandomSubdomain();
+  if (TUNNEL_SUBDOMAIN) {
+    return TUNNEL_SUBDOMAIN;
   }
 
-  return TUNNEL_SUBDOMAIN;
+  logger.warn('TUNNEL_SUBDOMAIN is missed, using a random subdomain');
+  return getRandomSubdomain();
 };
 
 const getPort = () => {
   const { PORT } = process.env;
 
-  if (!PORT) {
-    logger.warn('PORT is missed, using default 8080 port');
-    return '8080';
+  if (PORT) {
+    return PORT;
   }
 
-  return PORT;
+  logger.warn('PORT is missed, using default 8080 port');
+  return '8080';
 };
 
 require('dotenv').config();
-export const TUNNEL_SUBDOMAIN = getSubdomain();
-export const PORT = getPort();
+export const config = {
+  getSubdomain,
+  getPort,
+};
